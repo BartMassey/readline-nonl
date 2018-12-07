caaafed5f770db59ec84ca147c4ff8be5e5dd31d
Bart Massey <bart@cs.pdx.edu>
2018-12-07 08:48:31 -0800

    swizzled languages in test script

--- test.sh
+++ test.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 make -s
 cat <<'EOF' |
-d8 r.js
 ./r-c
 ./r-cc
 java r_java
 ./r-rs
-go run r.go
 runghc r.hs
 perl r.pl
 sh r.sh
 python3 r3.py
 python2 r2.py
+smjs r.js
+nickle r.5c
+go run r.go
+d8 r.js
 EOF
 while read CMD
 do
